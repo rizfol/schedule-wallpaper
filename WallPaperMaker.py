d048e770e58f51eee82494352eada938252dcb81
rizfol <yawesome23@gmail.com>
2024-09-29 00:08:02 -0400

    previous oops

--- WallPaperMaker.py
+++ WallPaperMaker.py
@@ -3,6 +3,7 @@ import numpy as np
 import pymupdf
 import os
 from dotenv import load_dotenv, dotenv_values
+from PIL import Image
 
 def convert_pdf_to_png(pdf_path):
     doc = pymupdf.open(pdf_path) # open a document
@@ -135,3 +136,31 @@ load_dotenv()
 file_path = os.getenv("FILE_PATH")
 convert_pdf_to_png(file_path+".pdf")
 box_extraction(file_path+".png", "Output/")
+
+width = int(os.getenv("SCREEN_WIDTH"))
+height = int(os.getenv("SCREEN_HEIGHT"))
+
+# joins the extracted images together
+idx = 0
+my_list = os.listdir("Output")
+my_list.sort()
+my_list.pop(0)
+# putting Output/ in front makes it so that Image.open has the correct relative path
+my_list = [f"Output/{i}" for i in my_list]
+
+images = [Image.open(x) for x in my_list]
+
+times_widths, times_heights = images[-1].size[0], images[-1].size[1]
+
+
+for im in images[:-1]:
+    idx += 1
+    new_im_width = times_widths + im.size[0]
+    new_im = Image.new("RGB", (new_im_width, times_heights))
+    new_im.paste(images[-1], (0, 0))
+    new_im.paste(im, (times_widths,0))
+    # new_im.save(f'Joined/{idx}.png', format="PNG")
+    background = Image.new('RGBA', (width, height), (255, 255, 255, 255))
+    offset = (int(round(((width - new_im_width) / 2), 0)), int(round(((height - times_heights)/2) + int(0.05 * height), 0)))
+    background.paste(new_im, offset)
+    background.save(f'Joined/{idx}.png', format="PNG")
